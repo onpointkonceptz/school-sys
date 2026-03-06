79946f24a105cae287f169b31dfa57dc45bec435
OnPoint Konceptz <onpointkonceptz@gmail.com>
2026-03-06 23:50:07 +0100

    Automate superuser creation on startup to remove need for terminal access

--- create_superuser.py
+++ create_superuser.py
@@ -2,7 +2,7 @@ import os
 import django
 from django.contrib.auth import get_user_model
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_sys.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
 User = get_user_model()
